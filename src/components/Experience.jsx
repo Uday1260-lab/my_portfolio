0bf784e24a6e1cb61c9481cf2705599845095256
Uday Prasad <Uday1260-lab>
2025-03-02 11:08:23 +0530

    tweak2

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -20,7 +20,7 @@ const ExperienceCard = ({ experience }) => {
         color: "#fff",
       }}
       contentArrowStyle={{ borderRight: "7px solid  #232631" }}
-      date={experience.date}
+      
       iconStyle={{ background: experience.iconBg }}
       icon={
         <div className='flex justify-center items-center w-full h-full'>
@@ -34,12 +34,12 @@ const ExperienceCard = ({ experience }) => {
     >
       <div>
         <h3 className='text-white text-[24px] font-bold'>{experience.title}</h3>
-        <p
+        {/* <p
           className='text-secondary text-[16px] font-semibold'
           style={{ margin: 0 }}
         >
           {experience.company_name}
-        </p>
+        </p> */}
       </div>
 
       <ul className='mt-5 list-disc ml-5 space-y-2'>
