35095dd5c1cec10548601a7dccef024e0976232d
Uday Prasad <Uday1260-lab>
2025-03-16 20:12:29 +0530

    Tweak 9

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,14 +1,34 @@
-import React from "react";
+// import React from "react";
 
-import { BallCanvas } from "./canvas";
+// import { BallCanvas } from "./canvas";
+// import { SectionWrapper } from "../hoc";
+// import { technologies } from "../constants";
+
+// const Tech = () => {
+//   return (
+//     <div className='flex flex-row flex-wrap justify-center gap-10'>
+//       {technologies.map((technology) => (
+//         <div className='w-28 h-28' key={technology.name}>
+//           <BallCanvas icon={technology.icon} />
+//         </div>
+//       ))}
+//     </div>
+//   );
+// };
+
+// export default SectionWrapper(Tech, "");
+
+
+import React from "react";
+import BallCanvas from "./canvas/Ball"; // Import the merged file
 import { SectionWrapper } from "../hoc";
 import { technologies } from "../constants";
 
 const Tech = () => {
   return (
-    <div className='flex flex-row flex-wrap justify-center gap-10'>
+    <div className="flex flex-row flex-wrap justify-center gap-10">
       {technologies.map((technology) => (
-        <div className='w-28 h-28' key={technology.name}>
+        <div className="w-28 h-28" key={technology.name}>
           <BallCanvas icon={technology.icon} />
         </div>
       ))}
---
+++ src/components/canvas/BallCanvas.jsx
@@ -0,0 +1,24 @@
+import React, { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Preload } from "@react-three/drei";
+import Ball from "./Ball";
+import CanvasLoader from "../Loader";
+
+const BallCanvas = ({ icon }) => {
+  return (
+    <Canvas
+      frameloop="demand" // Changed from 'always' to reduce load
+      dpr={[1, 2]}
+      gl={{ preserveDrawingBuffer: true }}
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls enableZoom={false} />
+        {/* Ball is SAFELY inside Canvas */}
+        <Ball imgUrl={icon} />
+      </Suspense>
+      <Preload all />
+    </Canvas>
+  );
+};
+
+export default BallCanvas;
